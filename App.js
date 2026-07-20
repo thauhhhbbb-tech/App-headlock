ba2984b4977fcdc4034ef4eeb826afca00fc05ea
thauhhhbbb-tech <thauhhhbbb@gmail.com>
2026-07-20 13:33:17 +0700

    Initialize React Native app with navigation

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import AppNavigator from './src/navigation/AppNavigator';
+
+export default function App() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </GestureHandlerRootView>
+  );
+}
